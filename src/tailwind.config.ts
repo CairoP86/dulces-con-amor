56b10c35edb06fd985db9904ba50c6e8a0a24b26
Cairo Pérez <capl182@hotmail.com>
2025-08-30 17:56:00 -0600

    update

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,13 +1,24 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        verde: "#4CAF50",
+        turquesa: "#40E0D0",
+        rosado: "#FF69B4",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
